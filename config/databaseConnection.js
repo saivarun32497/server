c83587c4c2e0c34f8a03a02fd7abaa8787a1f9a2
varun <saivarun@gmail.com>
2024-06-11 21:36:20 +0530

    modified:   config/databaseConnection.js

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async()=>{
-        const connection = mongoose.connect("mongodb+srv://saitharakreddyv59:tharak@tharak.k1lxca6.mongodb.net/bayata2")
+        const connection = mongoose.connect("mongodb+srv://saivarun3241:varun@cluster0.rumxkm1.mongodb.net/sip")
         .then(() => console.log("database connected"))
         .catch((err)=>console.log(err));
 }
